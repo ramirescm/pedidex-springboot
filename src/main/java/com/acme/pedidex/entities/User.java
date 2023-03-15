7389ef425be9ee1ca0f7c483ffca6239d1229df8
ramires marques <ramiresmarques@ramirescm.local>
2023-03-15 18:58:03 -0300

    added order and relation with user

--- src/main/java/com/acme/pedidex/entities/User.java
+++ src/main/java/com/acme/pedidex/entities/User.java
@@ -1,12 +1,11 @@
 package com.acme.pedidex.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "user")
@@ -18,6 +17,8 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -68,6 +69,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
